2c72a5c477f45612ec5af08a5bbd989337df51c6
Vasilena Dragancheva <vasilena86@hotmail.com>
2016-05-09 15:21:41 +0300

    Added Solutions for Royal Accounting and Royal Flush

---
+++ RoyalAccounting/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace RoyalAccounting
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text.RegularExpressions;
+
+    public class Employee
+    {
+        public Employee(string name, int hours, decimal payment)
+        {
+            this.Name = name;
+
+            this.Hours = hours;
+            this.Payment = payment;
+        }
+
+        public string Name { get; set; }
+
+        public string Team { get; set; }
+
+        public int Hours { get; set; }
+
+        public decimal Payment { get; set; }
+
+        public decimal MonthlyPayment
+        {
+            get
+            {
+                return this.DailyIncome * 30;
+            }
+        }
+
+        public decimal DailyIncome
+        {
+            get
+            {
+                return ((this.Payment * this.Hours) / 24);
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+            var employees = new Dictionary<string, List<Employee>>();
+
+            var pattern = @"^([a-zA-Z]+)\;(\d+)\;(\d*.\d*)\;([a-zA-Z]+)$";
+            var regex = new Regex(pattern);
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input.Contains("Pishi kuf"))
+                {
+                    break;
+                }
+
+                if (!regex.IsMatch(input))
+                {
+                    continue;
+                }
+
+                var matches = regex.Match(input);
+                var name = matches.Groups[1].ToString();
+                var hours = int.Parse(matches.Groups[2].ToString());
+                var m = matches.Groups[3].ToString();
+                var money = decimal.Parse(matches.Groups[3].ToString());
+                var team = matches.Groups[4].ToString();
+
+                if (employees.Any(t => t.Value.Any(em => em.Name == name)))
+                {
+                    continue;
+                }
+
+                var employee = new Employee(name, hours, money);
+                if (!employees.ContainsKey(team))
+                {
+                    employees.Add(team, new List<Employee>());
+                }
+                employees[team].Add(employee);
+            }
+            var orderedTeams = employees.OrderByDescending(t => t.Value.Sum(em => em.MonthlyPayment));
+
+            foreach (var orderedTeam in orderedTeams)
+            {
+                Console.WriteLine("Team - {0}", orderedTeam.Key);
+                var empl =
+                    orderedTeam.Value.OrderByDescending(em => em.Hours)
+                        .ThenByDescending(em => em.DailyIncome)
+                        .ThenBy(em => em.Name, StringComparer.Ordinal);
+                foreach (var em in empl)
+                {
+                    Console.WriteLine("$$${0} - {1} -{2:f6}", em.Name, em.Hours, em.DailyIncome);
+                }
+            }
+
+        }
+    }
+}
---
+++ RoyalFlush/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace RoyalFlush
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class Card
+    {
+        public Card(string suit, string value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+
+        public string Suit { get; set; }
+
+        public string Value { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var valueComparer = new Dictionary<string, int>();
+            var allCards = new List<Card>();
+
+            var suits = new Dictionary<string, string>
+                            {
+                                { "s", "Spades" }, 
+                                { "h", "Hearts" }, 
+                                { "c", "Clubs" }, 
+                                { "d", "Diamonds" }
+                            };
+
+            for (int i = 2; i <= 10; i++)
+            {
+                valueComparer.Add(i.ToString(), i);
+            }
+
+            valueComparer.Add("J", 11);
+            valueComparer.Add("Q", 12);
+            valueComparer.Add("K", 13);
+            valueComparer.Add("A", 14);
+            var input = new StringBuilder();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                input.Append(Console.ReadLine());
+            }
+
+            string line = input.ToString().Trim();
+            var regex = new Regex(@"([0-9]{1,2}|[JQKA])(s|d|c|h)");
+            MatchCollection matches = regex.Matches(line);
+            foreach (Match match in matches)
+            {
+                allCards.Add(new Card(match.Groups[2].ToString(), match.Groups[1].ToString()));
+            }
+
+            int counter = 0;
+            for (int i = 0; i < allCards.Count; i++)
+            {
+                var currentCard = allCards[i];
+
+                if (currentCard.Value == "10")
+                {
+                    var flush = new Stack<Card>();
+                    flush.Push(currentCard);
+
+                    for (int j = i + 1; j < allCards.Count; j++)
+                    {
+                        var lastCard = flush.Peek();
+                        var nextCard = allCards[j];
+                        if (lastCard.Suit == nextCard.Suit)
+                        {
+                            var difference = valueComparer[nextCard.Value] - valueComparer[lastCard.Value];
+                            if (difference == 1)
+                            {
+                                flush.Push(nextCard);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        if (flush.Count == 5)
+                        {
+                            Console.WriteLine("Royal Flush Found – {0}", suits[currentCard.Suit]);
+                            counter++;
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Royal’s Royal Flushes – {0}", counter);
+        }
+    }
+}
